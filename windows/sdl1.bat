a5dccb67c4955c9851ff94c5117bbd3dd404bfdd
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-07-20 01:49:26 +0300

    Style nits

--- windows/sdl1.bat
+++ windows/sdl1.bat
@@ -15,9 +15,9 @@ set SDL_MIXER_FILE=SDL_mixer-devel-1.2.12-VC.zip
 set SDL_URL=https://www.libsdl.org/release/%SDL_FILE%
 set SDL_MIXER_URL=https://www.libsdl.org/projects/SDL_mixer/release/%SDL_MIXER_FILE%
 
-echo Downloading %SDL_URL%...                                                                                      && ^
+echo Downloading %SDL_URL%                                                                                         && ^
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%SDL_URL%',       '%TEMP%\%SDL_FILE%')"       && ^
-echo Downloading %SDL_MIXER_URL%...                                                                                && ^
+echo Downloading %SDL_MIXER_URL%                                                                                   && ^
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%SDL_MIXER_URL%', '%TEMP%\%SDL_MIXER_FILE%')" || ^
 exit /B 1
 
@@ -30,7 +30,7 @@ call :unpack_archive "%TEMP%\%SDL_FILE%"           "%TEMP%"         && ^
 call :unpack_archive "%TEMP%\%SDL_MIXER_FILE%"     "%TEMP%"         || ^
 exit /B 1
 
-echo Copying files...
+echo Copying files
 
 xcopy /Y /S /Q "%TEMP%\SDL-1.2.15\include"       "%OUTPUT_DIR%\include\SDL" && ^
 xcopy /Y /S /Q "%TEMP%\SDL-1.2.15\lib"           "%OUTPUT_DIR%\lib"         && ^
@@ -42,7 +42,7 @@ exit /B
 
 :unpack_archive
 
-echo Unpacking %~n1%~x1...
+echo Unpacking %~n1%~x1
 
 if "%CI%" == "true" (
     powershell -Command "Expand-Archive -LiteralPath '%~1' -DestinationPath '%~2' -Force" || exit /B 1
