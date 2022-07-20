01d5280d15836f1591ec0a4f88020992ce3d986e
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2022-07-20 03:03:33 +0300

    Build dynamic zlib

--- windows/zlib.bat
+++ windows/zlib.bat
@@ -9,7 +9,7 @@ set VCPKG_DIR=%~1
 set PLATFORM=%~2
 set OUTPUT_DIR=%~3
 
-set TRIPLET=%PLATFORM%-windows-static
+set TRIPLET=%PLATFORM%-windows
 
 echo Building zlib...
 
@@ -21,9 +21,12 @@ if not exist "%OUTPUT_DIR%\lib\%PLATFORM%" ( mkdir "%OUTPUT_DIR%\lib\%PLATFORM%"
 
 echo Copying files...
 
+xcopy /Y /Q "%VCPKG_DIR%\installed\%TRIPLET%\bin\zlib*.dll" "%OUTPUT_DIR%\lib\%PLATFORM%" || ^
+exit /B 1
+
 xcopy /Y /Q "%VCPKG_DIR%\installed\%TRIPLET%\include\zconf.h" "%OUTPUT_DIR%\include" && ^
 xcopy /Y /Q "%VCPKG_DIR%\installed\%TRIPLET%\include\zlib.h"  "%OUTPUT_DIR%\include" || ^
 exit /B 1
 
-xcopy /Y /Q "%VCPKG_DIR%\installed\%TRIPLET%\lib\zlib.lib" "%OUTPUT_DIR%\lib\%PLATFORM%\zlibstatic.lib"* || ^
+xcopy /Y /Q "%VCPKG_DIR%\installed\%TRIPLET%\lib\zlib*.lib" "%OUTPUT_DIR%\lib\%PLATFORM%" || ^
 exit /B 1
